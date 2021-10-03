5f48fa451a9221379239ca97aeeb9e7715ba8913
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-10-03 21:30:55 +0700

    class manusia

---
+++ Intro/Manusia.dart
@@ -0,0 +1,20 @@
+class Manusia{
+  //field
+  String? nama;
+  int? usia;
+  String? alamat;
+
+  //method / behavior
+  void setNama(String nama){
+    this.nama = nama;
+  }
+
+  void setUsia(int usia){
+    this.usia = usia;
+  }
+
+  void setAlamat(String alamat){
+    this.alamat = alamat;
+  }
+
+}
